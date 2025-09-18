6f5479bfc32d7e8dbd5f1167401f0d3a8fe2599c
Adarsh <adarshs8400@gmail.com>
2025-09-18 10:27:04 +0530

    page router setup

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,12 @@
-import React from 'react'
+
+import {Button} from "@/components/ui/button";
 
 const Page = () => {
   return (
-    <div>Page</div>
+    <div>
+        <h1 className="text-2xl underline">Welcome to saas app</h1>
+        <Button>Click me</Button>
+    </div>
   )
 }
 
