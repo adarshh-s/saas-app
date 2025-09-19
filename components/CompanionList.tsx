465a3956ecf42da4a6caa2c56f49943811e6f2b4
Adarsh <adarshs8400@gmail.com>
2025-09-19 11:23:40 +0530

    implement recent usage and cta in home page

--- components/CompanionList.tsx
+++ components/CompanionList.tsx
@@ -1,10 +1,95 @@
-import React from 'react';
+import {
+    Table,
+    TableBody,
+    TableCaption,
+    TableCell,
+    TableHead,
+    TableHeader,
+    TableRow,
+} from "@/components/ui/table"
+import {cn, getSubjectColor} from "@/lib/utils";
+import Link from "next/link";
+import Image from "next/image";
 
-const CompanionList = () => {
+interface CompanionListProps {
+    title: string;
+    companions?: Companion[];
+    classNames?: string;
+}
+
+const  CompanionList = ({title, companions, classNames}: CompanionListProps) => {
     return (
-        <div>
-            CompanionList
-        </div>
+        <article className={cn("companion-list", classNames)}>
+            <h2 className="font-bold text-3xl">Recent Usage</h2>
+            <Table>
+                {/*<TableCaption>A list of your recent invoices.</TableCaption>*/}
+                <TableHeader>
+                    <TableRow>
+                        <TableHead className="text-lg w-2/3">Lessons</TableHead>
+                        <TableHead className="text-lg">Subject</TableHead>
+                        <TableHead className="text-lg text-right">Duration</TableHead>
+                    </TableRow>
+                </TableHeader>
+                <TableBody>
+                    {companions?.map(({id,subject,name,topic,duration}) => (
+                        <TableRow key={id}>
+                            <TableCell>
+                                <Link href={`/companions/${id}`}>
+                                    <div className="flex items-center gap-3">
+                                        <div className="size-[72] flex items-center justify-center
+                                        rounded-lg max-md:hidden"
+                                        style={{backgroundColor: getSubjectColor(subject)}}>
+                                            <Image src={`/icons/${subject}.svg`}
+                                                   alt={subject}
+                                                   width={35}
+                                                   height={35}
+                                            />
+                                        </div>
+                                        <div className="flex flex-col gap-3">
+                                            <p className="font-bold text-2xl">
+                                                {name}
+                                            </p>
+                                            <p className="text-lg">
+                                                {topic}
+                                            </p>
+                                        </div>
+                                    </div>
+                                </Link>
+                            </TableCell>
+                            <TableCell>
+                                <div className="subject-badge w-fit max-md:hidden">
+                                    {subject}
+                                </div>
+                                <div className="flex items-center justify-center rounded-lg w-fit
+                                p-2 md:hidden"
+                                style={{backgroundColor:getSubjectColor(subject)}}>
+                                    <Image src={`/icons/${subject}.svg`}
+                                           alt={subject}
+                                           width={18}
+                                           height={18}
+                                           />
+                                </div>
+                            </TableCell>
+                            <TableCell>
+                                <div className="flex items-center gap-3 justify-end w-full">
+                                    <p className="text-2xl">
+                                        {duration} { " "}
+                                        <span className="max-md:hidden">mins</span>
+                                    </p>
+                                    <Image
+                                        src="/icons/clock.svg"
+                                        alt="minutes"
+                                        width={14}
+                                        height={14}
+                                        className="md:hidden"
+                                        />
+                                </div>
+                            </TableCell>
+                        </TableRow>
+                    ))}
+                </TableBody>
+            </Table>
+        </article>
     );
 };
 
