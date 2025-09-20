da649867bcc31016570587ed44e67d9d60c0d792
Adarsh <adarshs8400@gmail.com>
2025-09-20 09:55:52 +0530

    implement clerk authentication functionality

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,14 @@ import type { Metadata } from "next";
 import { Bricolage_Grotesque } from "next/font/google";
 import "./globals.css";
 import Navbar from "@/components/Navbar";
+import {
+  ClerkProvider,
+  SignInButton,
+  SignUpButton,
+  SignedIn,
+  SignedOut,
+  UserButton,
+} from "@clerk/nextjs";
 
 const bricolage = Bricolage_Grotesque({
   variable: "--font-bricolage",
@@ -19,11 +27,22 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body className={`${bricolage.variable} antialiased`}>
-      <Navbar/>
-      {children}
-      </body>
-    </html>
+      <html lang="en">
+        <body className={`${bricolage.variable} antialiased`}>
+        <ClerkProvider appearance={{variables:{colorPrimary:'#fe5933'}}}>
+          {/*<header className="p-4 border-b flex gap-2 items-center justify-end">*/}
+          {/*  <SignedOut>*/}
+          {/*    <SignInButton />*/}
+          {/*    <SignUpButton />*/}
+          {/*  </SignedOut>*/}
+          {/*  <SignedIn>*/}
+          {/*    <UserButton />*/}
+          {/*  </SignedIn>*/}
+          {/*</header>*/}
+          <Navbar />
+          {children}
+         </ClerkProvider>
+        </body>
+      </html>
   );
 }
