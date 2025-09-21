cfe6ee28106e4a5296cc857a32faff468f95a198
Adarsh <adarshs8400@gmail.com>
2025-09-21 16:39:05 +0530

    implement search input and subject filter

--- components/SearchInput.tsx
+++ components/SearchInput.tsx
@@ -1,8 +1,9 @@
 'use client'
 
-import React, {useState} from 'react';
+import React, {useEffect, useState} from 'react';
 import {usePathname, useSearchParams,useRouter} from "next/navigation";
 import Image from "next/image";
+import {formUrlQuery, removeKeysFromUrlQuery} from "@jsmastery/utils";
 
 const SearchInput = () => {
     const pathName = usePathname();
@@ -11,6 +12,34 @@ const SearchInput = () => {
     const query = searchParams.get('topic') || ''
 
     const [searchQuery, setSearchQuery] = useState('');
+
+    useEffect(() =>{
+        const delayDebounceFn = setTimeout(() => {
+
+        if(searchQuery) {
+            const newUrl = formUrlQuery({
+                params: searchParams.toString(),
+                key: "topic",
+                value: searchQuery,
+            });
+            router.push(newUrl, { scroll: false });
+        }else{
+            if(pathName === '/companions'){
+                const newUrl = removeKeysFromUrlQuery({
+                    params: searchParams.toString(),
+                    keysToRemove: ["topic"],
+                });
+
+                router.push(newUrl, { scroll: false });
+            }
+        }
+        },500)
+
+
+
+
+    }, [searchQuery,pathName,router,searchParams]);
+
     return (
         <div className="relative border border-black rounded-lg items-center flex gap-2
         px-2 py-1 h-fit">
