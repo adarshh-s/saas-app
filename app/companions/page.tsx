a0fab708c82e5747fe630ac03be5d36bd5e40748
Adarsh <adarshs8400@gmail.com>
2025-09-25 16:03:54 +0530

    implement bookmark

--- app/companions/page.tsx
+++ app/companions/page.tsx
@@ -4,31 +4,33 @@ import {getSubjectColor} from "@/lib/utils";
 import SearchInput from "@/components/SearchInput";
 import SubjectFilter from "@/components/SubjectFilter";
 
+const CompanionsLibrary = async ({ searchParams }: SearchParams) => {
+    const filters = await searchParams;
+    const subject = filters.subject ? filters.subject : '';
+    const topic = filters.topic ? filters.topic : '';
 
-const CompanionsLibrary = async ({searchParams}: SearchParams) => {
-   const filters  = await searchParams;
-   const subject = filters.subject? filters.subject : '';
-   const topic = filters.topic? filters.topic: '';
+    const companions = await getAllCompanions({ subject, topic });
 
-   const companions = await getAllCompanions({subject,topic})
-    console.log(companions)
     return (
         <main>
             <section className="flex justify-between gap-4 max-sm:flex-col">
                 <h1>Companion Library</h1>
                 <div className="flex gap-4">
-                    <SearchInput/>
-                    <SubjectFilter/>
+                    <SearchInput />
+                    <SubjectFilter />
                 </div>
             </section>
             <section className="companions-grid">
-                {companions.map((companion)=>(
-                    <CompanionCard key={companion.id} {...companion}
-                    color={getSubjectColor(companion.subject)}/>
+                {companions.map((companion) => (
+                    <CompanionCard
+                        key={companion.id}
+                        {...companion}
+                        color={getSubjectColor(companion.subject)}
+                    />
                 ))}
             </section>
         </main>
-    );
-};
+    )
+}
 
-export default CompanionsLibrary;
+export default CompanionsLibrary
